39a9e07e5d736f3e9e54ff8e68ea1216f317c804
edwardraymondhe <edward.raymond.he@hotmail.com>
2020-10-04 18:24:10 +0800

    feat: basic scripts for multiplayer

---
+++ Endless Memories/Assets/Scripts/PVP/PlayerSpawner.cs
@@ -0,0 +1,19 @@
+﻿using Cinemachine;
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject playerPrefab = null;
+    [SerializeField] private CinemachineFreeLook tpsCamera = null;
+    [SerializeField] private CinemachineVirtualCamera vrCamera = null;
+
+    private void Start()
+    {
+        // var player = PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity);
+        vrCamera.Follow = player.transform;
+        vrCamera.LookAt = player.transform;
+    }
+}
