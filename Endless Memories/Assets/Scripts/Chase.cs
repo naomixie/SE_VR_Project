35785e0f9676098fc9cb6432a9941665dda7a538
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2020-10-14 11:34:11 +0800

    feat: Ghost has teleport functions(including stun after teleport)

--- Endless Memories/Assets/Scripts/Chase.cs
+++ Endless Memories/Assets/Scripts/Chase.cs
@@ -11,21 +11,58 @@ public class Chase : MonoBehaviour
     public Transform target;
     //寻路组件
     private NavMeshAgent agent;
+    //stun when used teleport
+    private const int STUN_FRAMES = 300;
+    private int lestStunFrame;
 
     private void Start()
     {
+        target = GameObject.FindGameObjectWithTag("First Person Player").transform;
+        //获取寻路物体上的NavMeshAgent组件, 通过SetDestination方法(网格路径计算)实现自动寻路
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Use this for initialization
     void Update()
     {
-        if(target == null)
+        if (lestStunFrame > 0)
         {
-        target = GameObject.FindGameObjectWithTag("First Person Player").transform;
+            --lestStunFrame;
+            agent.isStopped = true;
         }
-        //获取寻路物体上的NavMeshAgent组件
-        agent = GetComponent<NavMeshAgent>();
-        //通过SetDestination方法(网格路径计算)实现自动寻路
-        agent.SetDestination(target.position);
+        else
+        {
+            agent.isStopped = false;
+            //Need always updates latest position
+            agent.SetDestination(target.position);
+        }
+    }
+    public void teleport(Vector3 position)
+    {
+        agent.Warp(position);
+        lestStunFrame = STUN_FRAMES;
+    }
+    /**
+     *  teleport to the object which tag is GhostTeleportSpot and name ends with specific strings
+     *  teleport("1FC"); //usage
+     */
+    public void teleport(string ghostTeleportSpotNameSuffix)
+    {
+        foreach(GameObject gameObject in GameObject.FindGameObjectsWithTag("GhostTeleportSpot"))
+        {
+            if (gameObject.name.EndsWith(ghostTeleportSpotNameSuffix))
+            {
+                teleport(gameObject.transform.position);
+                break;
+            }
+        }
+    }
+    public void teleportRandomSpot()
+    {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("GhostTeleportSpot");
+        if (gameObjects.Length == 0)
+            return;
+        int targetId = Random.Range(0, gameObjects.Length - 1);
+        teleport(gameObjects[targetId].transform.position);
     }
 }
