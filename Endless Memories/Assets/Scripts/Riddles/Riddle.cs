df7aa48f069fe1264dd3ce0fd296a7ce0a07457a
edwardraymondhe <edward.raymond.he@hotmail.com>
2020-10-17 14:59:18 +0800

    feat: created logic for basic riddleManager

---
+++ Endless Memories/Assets/Scripts/Riddles/Riddle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Riddle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string riddleName = "Riddle";
+    
+    // 谜题种类：顺序执行，乱序执行
+    public int riddleType = 1;
+
+    // 所有谜题的状态数值从 1 开始，即 第一关
+    // 部分谜题将只包含 “完成”/“未完成”
+    // 部分谜题将包含其他谜题
+    public int riddleStatus = 1;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public virtual int RiddleDetail()
+    {
+        // 把所有的谜题详情写在该类的子类中
+        return 0;
+    }
+}
