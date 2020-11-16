f139b79d2cbbf02b3ae1f14f9e08d366bad361e0
Naomi <41462684+naomixie@users.noreply.github.com>
2020-11-16 11:48:16 +0800

    Revert "Merge branch 'master' into Naomi-backup"
    This reverts commit b0c74a801a7809ccc89c45a58f921c38d7823ca5, reversing
    changes made to 5c1cab3c30d56e7dd09c44376b6a58c1c8f79c69.

--- Endless Memories/Assets/Scripts/MouseLook.cs
+++ Endless Memories/Assets/Scripts/MouseLook.cs
@@ -23,18 +23,25 @@ public class MouseLook : MonoBehaviourPun
     // Update is called once per frame
     void Update()
     {
+
         if(solo == 1 || photonView.IsMine)
         {
-            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-            // Debug.Log(mouseY);
-            xRotation -= mouseY;
-            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-            playerBody.Rotate(Vector3.up * mouseX);
+            ProcessInput();
         }
+        
+    }
+
+    void ProcessInput()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Debug.Log(mouseY);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 
     public void UnlockMouse ()
