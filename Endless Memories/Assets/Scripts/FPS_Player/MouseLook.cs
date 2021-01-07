889649e371a4aa735fc134563c10443f195bc96a
edwardraymondhe <43778680+edwardraymondhe@users.noreply.github.com>
2021-01-07 20:37:53 +0800

    some changes

--- Endless Memories/Assets/Scripts/FPS_Player/MouseLook.cs
+++ Endless Memories/Assets/Scripts/FPS_Player/MouseLook.cs
@@ -10,16 +10,19 @@ public class MouseLook : MonoBehaviourPun
 
     public Transform playerBody;
 
-    float xRotation = 0f;
+    float rotationX = 0f;
+    float rotationY = 0f;
 
     private int solo = 1;
 
     public float tmp = 1.0f;
+    Quaternion originalRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        originalRotation = transform.localRotation;
     }
 
     // Update is called once per frame
@@ -35,15 +38,15 @@ public class MouseLook : MonoBehaviourPun
 
     void ProcessInput()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        rotationX += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        rotationY += Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        // Debug.Log(mouseY);
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * tmp);
+        rotationY =  Mathf.Clamp (rotationY, -90f , 90f);
+        
+        Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
+        Quaternion yQuaternion = Quaternion.AngleAxis (rotationY, Vector3.left);
+        
+        transform.localRotation = originalRotation * xQuaternion * yQuaternion;
     }
 
     public void UnlockMouse ()
